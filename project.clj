dd5433a337145a15311b1c95dc95fecd4f85f339
mt3593 <markctinsley@gmail.com>
2018-09-11 09:40:14 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circleci-ultra-reporter "0.1.0-SNAPSHOT"
+(defproject circleci-ultra-reporter "0.1.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [circleci/circleci.test "0.4.1"]
                  [venantius/ultra "0.5.2"]]
